2556b012299ad80cd5a80d3356c0c8627844ebb0
fcjreed <fcjreed@gmail.com>
2019-01-21 20:54:26 +0000

    Fix issue with SystemTray and default port

--- src/main/java/poe/mod/manager/boot/ModManagerMain.java
+++ src/main/java/poe/mod/manager/boot/ModManagerMain.java
@@ -1,19 +1,7 @@
 package poe.mod.manager.boot;
 
-import java.awt.Desktop;
-import java.awt.MenuItem;
-import java.awt.PopupMenu;
-import java.awt.SystemTray;
-import java.awt.Toolkit;
-import java.awt.TrayIcon;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Import;
 
 import poe.mod.manager.config.ModManagerJavaConfig;
@@ -21,48 +9,10 @@ import poe.mod.manager.config.ModManagerJavaConfig;
 @SpringBootApplication
 @Import(ModManagerJavaConfig.class)
 public class ModManagerMain {
-	
-	private static final PopupMenu popup = new PopupMenu();
-	private static final TrayIcon trayIcon = new TrayIcon(Toolkit.getDefaultToolkit().getImage(System.getProperty("user.dir") + "\\src\\images\\icon.gif"));
-	private static final SystemTray tray = SystemTray.getSystemTray();
-	private static final MenuItem launchApp = new MenuItem("Goto Mod Manager");
-	private static final MenuItem exit = new MenuItem("Exit");
 
 	public static void main(String...args) {
-		SpringApplication.run(ModManagerMain.class, args);
-		initSysTray();
-	}
-	
-	public static void initSysTray() {
-		if (SystemTray.isSupported()) {
-			exit.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					tray.remove(trayIcon);
-					System.exit(0);
-				}
-			});
-			launchApp.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					try {
-						Desktop.getDesktop().browse(new URI("http://localhost:8080"));
-					} catch (Exception e1) {
-						e1.printStackTrace();
-					}
-				}
-			});
-			popup.add(launchApp);
-			popup.addSeparator();
-			popup.add(exit);
-			trayIcon.setPopupMenu(popup);
-			try {
-				trayIcon.setImageAutoSize(true);
-				tray.add(trayIcon);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(ModManagerMain.class);
+		builder.headless(false).run(args);
 	}
 	
 }
---
+++ src/main/java/poe/mod/manager/boot/SystemTrayInit.java
@@ -0,0 +1,73 @@
+package poe.mod.manager.boot;
+
+import java.awt.Desktop;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SystemTrayInit implements ApplicationListener<ApplicationReadyEvent> {
+
+	private PopupMenu popup;
+	private TrayIcon trayIcon;
+	private SystemTray tray;
+	private MenuItem launchApp;
+	private MenuItem exit;
+	@Value("${server.port}")
+	private int serverPort;
+	
+	public void initTrayIcon() {
+		if (SystemTray.isSupported()) {
+			popup = new PopupMenu();
+			trayIcon = new TrayIcon(Toolkit.getDefaultToolkit().getImage(System.getProperty("user.dir") + "\\src\\images\\icon.gif"));
+			tray = SystemTray.getSystemTray();
+			launchApp = new MenuItem("Goto Mod Manager");
+			exit = new MenuItem("Exit");
+			exit.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					tray.remove(trayIcon);
+					System.exit(0);
+				}
+			});
+			launchApp.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					try {
+						Desktop.getDesktop().browse(new URI("http://localhost:" + serverPort));
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+				}
+			});
+			popup.add(launchApp);
+			popup.addSeparator();
+			popup.add(exit);
+			trayIcon.setPopupMenu(popup);
+			try {
+				trayIcon.setImageAutoSize(true);
+				tray.add(trayIcon);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public SystemTrayInit() {}
+
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+		initTrayIcon();
+	}
+	
+}
